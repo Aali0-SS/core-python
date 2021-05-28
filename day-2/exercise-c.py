8589d0ddcdc7438876b6eac3d7e2ae99816a81c8
Ahmed Ali <ahmed.adil.ali@gmail.com>
2021-05-27 17:48:19 -0700

    exercise update

--- day-2/exercise-c.py
+++ day-2/exercise-c.py
@@ -1,14 +1,5 @@
 # Three is a Crowd
-# Throwing error need to fix
-def crowd_test(crowd, val):
-    for i in crowd:
-        if val>= i:
-            return False
-    return True
-
-crowd = ['Ahmed', 'Jessica', 'Yasin', 'Isa']
-val = 3
-if(crowd_test(crowd, val)):
-    print('The room is not too full')
-else:
-    print('The room is getting crowded')
+name_list = ['Bob', 'Tim', 'Sam', 'Jill' ]
+three_name  = ['a', 'b', 'c']
+if three_name > name_list:
+        print('The room is getting crowded')
